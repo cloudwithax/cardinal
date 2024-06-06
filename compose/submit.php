712b77c222ac61c69f96168f9c1f98771f2740ea
cloudwithax <heyimnick14@gmail.com>
2024-06-06 16:32:21 -0400

    init commit

--- compose/submit.php
+++
@@ -1,7 +0,0 @@
-<?php
-$myfile = fopen("chirp.json", "w") or die("Unable to open file!");
-$chirp = array("status"=>$_POST['chirpComposeText'], "user"=>"blank", "timestamp"=>time());
-fwrite($myfile, json_encode($chirp));
-fclose($myfile);
-header('Location: ../chirp');
-?>
